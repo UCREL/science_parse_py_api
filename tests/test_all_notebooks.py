856a29defeabcdbeb70f8161262a06b17edd7e88
Andrew Moore <andrew.p.moore94@gmail.com>
2021-03-09 14:50:39 +0000

    Tests relative path and code coverage

--- tests/test_all_notebooks.py
+++ tests/test_all_notebooks.py
@@ -1,5 +1,5 @@
 import nbdev.test
 
 def test_run():
-    nbdev.test.test_nb('/home/andrew/science_parse_py_api/module_notebooks/00_api.ipynb')
-    nbdev.test.test_nb('/home/andrew/science_parse_py_api/module_notebooks/01_test_helper.ipynb')
+    nbdev.test.test_nb('./module_notebooks/00_api.ipynb')
+    nbdev.test.test_nb('./module_notebooks/01_test_helper.ipynb')
